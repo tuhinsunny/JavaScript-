be6e2ec3ea78d17456c3de9030abe917a89dcb74
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2024-06-27 14:39:17 +0530

    DataTypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS codes as newer version
+
+// alert("Hello") // we are using nodejs and not browser
+
+console.log(3
+    +
+    3) // code readability should be high . We should not write like this
+
+console.log('Tuhin');     
+
+
+let name = "tuhin" //string
+let age = 18 // number 
+let isLoggedIn = false // boolean
+
+// number =>2 to the power 53
+//bigInt
+// string => " "
+// boolean => true/false
+//null => standalone value
+//undefined
+//symbol => used to find uniqueness 
+
+//object
+
+console.log(typeof "Tuhin");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
