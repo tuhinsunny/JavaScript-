d2947f18ff00e831f3f80c9684c50f5b38d4db8b
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2024-06-30 15:35:07 +0530

    Added few more scopes and hoisting concepts

--- 03_basics/03_Scopes.js
+++ 03_basics/03_Scopes.js
@@ -1,3 +1,4 @@
+/*
 // {}-> Scope of the function
 // var c = 300
 let a = 300
@@ -13,3 +14,40 @@ if (true) {
 console.log(a); //300
 // console.log(b); // cannot be used outside the scope
 // console.log(c); //30
+*/
+
+function one(){
+    const username = "Tuhin"
+
+    function two(){
+        const website = "youtube"
+        console.log(username); //Tuhin
+    }
+    // console.log(website); // will show error as website is out of scope
+    two()
+}
+one()
+
+if(true){
+    const username = "Tuhin Chandra"
+    if(username === "Tuhin Chandra"){
+        const website = " youtube"
+        console.log(username+website); //Tuhin Chandra youtube
+    }
+    // console.log(website); //will give error
+}
+// console.log(username); // will give error as username is not in scope
+
+
+// ++++++++++++++++++++++ Interesting ++++++++++++++++++++++++++ 
+
+console.log(addone(5)); //6
+function addone(num){
+    return num + 1
+}
+
+addTwo(7) //will give error -> We are accessing before declaration
+const addTwo = function(num){
+    return num+2
+}
+// Hoisting is JavaScript's default behavior of moving declarations to the top.
